7979bd197e41330bc1627c2fbb3c7248597cda75
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-04-11 18:43:32 +0200

    updated loading of data

--- src/main.py
+++ src/main.py
@@ -9,7 +9,18 @@ import numpy as np
 # Opening files
 def openData(filename):
     return np.loadtxt(os.path.join("resources", filename), comments="%")
-# List containg all data
-file = openData("MSIS.dat")
+# Loading data
+msisFile = openData("MSIS.dat")
+iriFile = openData("IRI.dat")
+# Loading data for height and densities
+height = msisFile[:, 0:1]
+nO2 = None
+nO = None
+nN2 = None
+
+
+
+
+
 
 
