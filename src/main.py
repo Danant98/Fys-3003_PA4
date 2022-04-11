1c1f902d9649240112c152b8b43f2f56823a4e09
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-04-11 17:18:53 +0200

    updated files and directory

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""
+@author: Daniel Elisabethsønn Antonsen, UiT Institute of Physics and Technology
+
+Main module assignment
+"""
+# Importing libraries and modules
+import os
+import numpy as np
+# Opening files
+
