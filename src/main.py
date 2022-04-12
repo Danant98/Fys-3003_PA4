42c436695c3a2792121c270391332d97c73ffaa1
Daniel <danieleantonsen@gmail.com>
2022-04-12 15:32:26 +0200

    updated main file

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ Main file for assignment
 # Importing libraries and modules
 import os
 import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 # Opening files
 def openData(filename):
     """
@@ -18,13 +20,15 @@ iriFile = openData("IRI.dat")
 # Loading data for height and densities
 height = msisFile[:, 0:1] # Height, km
 ne = iriFile[:, 1:2] # Electron density, m^(-3)
-nO2 = msisFile[:, 3:4] # Number density of molecular oxygen, cm^(-3)
-nO = msisFile[:, 1:2] # Number density of atomic oxygen, cm^(-3)
-nN2 = msisFile[:, 2:3] # Number density of molecular nitrogen, cm^(-3)
+nO2 = msisFile[:, 3:4] * (1/1E6) # Number density of molecular oxygen, m^(-3)
+nO = msisFile[:, 1:2] * (1/1E6) # Number density of atomic oxygen, m^(-3)
+nN2 = msisFile[:, 2:3] * (1/1E6) # Number density of molecular nitrogen, m^(-3)
 temperature = msisFile[:, 5:6] # Temperature, K
 
-
+# Defining the time using numpy array
+t = np.arange(0, 3600, 1) # Time, s
 
 
 if __name__ == '__main__':
-    print(temperature)
+    #plt.show()
+    pass
