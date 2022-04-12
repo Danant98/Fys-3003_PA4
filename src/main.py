a1629fd326f9132e38b9f7aa10980d658af6162f
Daniel <danieleantonsen@gmail.com>
2022-04-12 17:03:22 +0200

    updated main file

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ msisFile = openData("MSIS.dat")
 iriFile = openData("IRI.dat")
 # Loading data for height and densities
 height = msisFile[:, 0:1] # Height, km
-ne = iriFile[:, 1:2] # Electron density, m^(-3)
+ne = iriFile[:, 1:2] # Electron number density, m^(-3)
 nO2 = msisFile[:, 3:4] * (1/1E6) # Number density of molecular oxygen, m^(-3)
 nO = msisFile[:, 1:2] * (1/1E6) # Number density of atomic oxygen, m^(-3)
 nN2 = msisFile[:, 2:3] * (1/1E6) # Number density of molecular nitrogen, m^(-3)
@@ -27,6 +27,9 @@ temperature = msisFile[:, 5:6] # Temperature, K
 
 # Defining the time using numpy array
 t = np.arange(0, 3600, 1) # Time, s
+
+
+
 
 
 if __name__ == '__main__':
