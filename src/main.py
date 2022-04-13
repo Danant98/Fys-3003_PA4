8ed24287cec6d9b52886da7cde50ec1d862e3a00
Daniel <danieleantonsen@gmail.com>
2022-04-13 13:55:26 +0200

    loaded in additional data from file

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import numpy as np
 import scipy as sp
 import scipy.integrate as integral 
 import matplotlib.pyplot as plt
-# Opening files
+# Defining functions to open files containing the data
 def openData(filename):
     """
     Function to load in data from file
@@ -24,8 +24,16 @@ ne = iriFile[:, 1:2] # Electron number density, m^(-3)
 nO2 = msisFile[:, 3:4] * (1/1E6) # Number density of molecular oxygen, m^(-3)
 nO = msisFile[:, 1:2] * (1/1E6) # Number density of atomic oxygen, m^(-3)
 nN2 = msisFile[:, 2:3] * (1/1E6) # Number density of molecular nitrogen, m^(-3)
-temperature = msisFile[:, 5:6] # Temperature, K
-
+temperature = msisFile[:, 5:6] # Neutral temperature, K
+ionTemp = iriFile[:, 2:3] # Ion temperature, K
+electronTemp = iriFile[:, 3:4] # Electron temperature, K
+# Loading the amount of ions
+ionO = iriFile[:, 4:5]
+ionH = iriFile[:, 5:6]
+ionHe = iriFile[:, 6:7]
+ionO2 = iriFile[:, 7:8]
+ionNO = iriFile[:, 8:9]
+ionN = iriFile[:, 10:11]
 # Defining the time using numpy array
 t = np.arange(0, 3600, 1) # Time, s
  # Radiative recombination-rate, (m^3)/s
@@ -34,6 +42,8 @@ radiativeRecRate = 3.7E-18 * (250 / temperature)**(0.7)
 alpha1 = 2.1E-13 * (temperature / 300)**(-0.85)
 alpha2 = 1.9E-13 * (temperature / 300)**(-0.5)
 alpha3 = 1.8E-13 * (temperature / 300)**(-0.39)
+# Defining the average alpha
+#avgAlpha = (alpha1 * ())
 # 
 
 
