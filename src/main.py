6043ac68e99e8eb0a1fdcd9431384716259bb396
Daniel <danieleantonsen@gmail.com>
2022-04-20 22:56:05 +0200

    updated main file

--- src/main.py
+++ src/main.py
@@ -34,36 +34,47 @@ ionHe = iriFile[:, 6:7]
 ionO2 = iriFile[:, 7:8]
 ionNO = iriFile[:, 8:9]
 ionN = iriFile[:, 10:11]
-# Defining the time using numpy array
-t = np.arange(0, 3600, 1) # Time, s
+# Defining the time as a numpy array with spacing 1s
+t = np.linspace(0, 3600, 1) # Time, s
 # Defining the constant ionization-rate
 ionRate = 1E8
 # Integrating 
-def solveDiffContinutity(h, time):
+def solveDiffContinutity(time):
     """
-    Function to solve the differential equation for a given height 'h' and a given time-interval 'time'
+    Function to solve the differential equation for a given height 'h' and a given time-interval 'time' used as reference
 
     Input 'height' with reference height at 100 km and 'time'
     """
      # Radiative recombination-rate, (m^3)/s
-    radiativeRecRate = 3.7E-18 * (250 / electronTemp[h])**(0.7)
+    radiativeRecRate = 3.7E-18 * (250 / electronTemp)**(0.7)
     # Defining dissociate recombination reaction rates, (m^3)/s
-    alpha1 = 2.1E-13 * (electronTemp[h] / 300)**(-0.85)
-    alpha2 = 1.9E-13 * (electronTemp[h] / 300)**(-0.5)
-    alpha3 = 1.8E-13 * (electronTemp[h] / 300)**(-0.39)
+    alpha1 = 2.1E-13 * (electronTemp / 300)**(-0.85)
+    alpha2 = 1.9E-13 * (electronTemp / 300)**(-0.5)
+    alpha3 = 1.8E-13 * (electronTemp / 300)**(-0.39)
     # Defining the average alpha
-    avgAlpha = (alpha1 * (ionNO[h] / ne[h]) + alpha2 * (ionO2[h] / ne[h]) + alpha3 * (nN2[h] / ne[h]))
-    # Integrating
-    integralNee = integral.cumulative_trapezoid(ionRate - avgAlpha * (ne[h]) * (ne[h]), time)
-    return (integralNee)
+    avgAlpha = (alpha1 * (ionNO / ne) + alpha2 * (ionO2 / ne) + alpha3 * (nN2 / ne))
+    # Solving ODE
+    fun = ionRate - avgAlpha * (ne) * (ne)
+    odeSolve = integral.odeint(fun, time, height)
+    return (odeSolve)
 """
 Solving the continuity equations for two heights; approx 110km and 230km. Both integrated over 3600s.
 Is to be used as a stable background for initial conditions for the different densities.
 """ 
 # Solving the continuous equations using the function defined above
-height110km = solveDiffContinutity(10, t)
-height230km = solveDiffContinutity(130, t)
+print(solveDiffContinutity(t))
+exit()
+height230km = solveDiffContinutity(t)
+# Solving the continuous equations for ionization-puls over 100s
+def solveDiffFor100sIon():
+    """
+    
+    """
 
+
+    return None
+
+exit()
 # Plot
 plt.plot(height110km, t[:3599])
 plt.plot(height230km, t[:3599], 'r--')
