e38e156597c4bc975d235c2c228149de19ada292
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-06-02 18:35:35 +0200

    updated files

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ Main file for programming assignment 4; Ion Chemistry
 """
 # Importing libraries and modules
 import os
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 # Opening files
@@ -20,24 +21,30 @@ iriFile = openData("IRI.dat")
 
 # Loading data for height and densities
 height = msisFile[100:, 0:1] # Height, km
-nO2 = msisFile[100:, 3:4] * 1E6 # Number density molecular oxygen, m^-3
-nO = msisFile[100:, 1:2] * 1E6 # Number density atomic oxygen, m^-3
-nN2 = msisFile[100:, 2:3] * 1E6 # Number density molecular nitrogen, m^-3
+O2 = msisFile[100:, 3:4] * 1E6 # Number density molecular oxygen, m^-3
+O = msisFile[100:, 1:2] * 1E6 # Number density atomic oxygen, m^-3
+N2 = msisFile[100:, 2:3] * 1E6 # Number density molecular nitrogen, m^-3
 ne = iriFile[:, 1:2] # Number density electrons, m^-3
-nOplus = iriFile[:, 4:5] * ne/100 # Number density O ions, m^-3
-nO2plus = iriFile[:, 7:8] * ne/100 # Number density O2 ions, m^-3
-nNOplus = iriFile[:, 8:9] * ne/100 # Number density NO ions, m^-3
-nN2plus = 0 # Number density N2 ions, m^-3
-nNO = 0 # Number density NO, m^-3
-Ti = iriFile[:, 2:3] # Ion temperature, K
-Te = iriFile[:, 3:4] # Electron temperature, K
-Tn = msisFile[:, 5:6] # Neutral temperature, K
-Tr = (Tn + Ti) / 2
+Oplus = iriFile[:, 4:5] * ne/100 # Number density O ions, m^-3
+O2plus = iriFile[:, 7:8] * ne/100 # Number density O2 ions, m^-3
+NOplus = iriFile[:, 8:9] * ne/100 # Number density NO ions, m^-3
+N2plus = 0 # Number density N2 ions, m^-3
+NO = 0 # Number density NO, m^-3
+ti = iriFile[:, 2:3] # Ion temperature, K
+te = iriFile[:, 3:4] # Electron temperature, K
+tn = msisFile[100:, 5:6] # Neutral temperature, K
+tr = (tn + ti) / 2
 
-def odes(qe=1E8, x, t):
+def odes(x, t, h, qe):
     """
     Function defining the ODEs
     """
+    Te = te[int(h)]
+    Tr = tr[int(h)]
+    nO = O[int(h)]
+    nO2 = O2[int(h)]
+    nN2 = N2[int(h)]
+
     # Defining reactionrate (m^3/s)
     alpha1 = 2.1E-13 * (Te / 300)**(-0.85)
     alpha2 = 1.9E-13 * (Te / 300)**(-0.5)
@@ -52,11 +59,9 @@ def odes(qe=1E8, x, t):
     k6 = 5E-17 * (Tr / 300)**(-0.8)
 
     # Defining ionization-rate (/m^3/s)
-    qe = 1E8
     qN2plus = qe * (0.92 * nN2) / (0.92*nN2 + nO2 + 0.56*nO)
     qOplus = qe * (0.56 * nO) / (0.92*nN2 + nO2 + 0.56*nO)
     qO2plus = qe * (nO2) / (0.92*nN2 + nO2 + 0.56*nO)
-    qN2plus = qe * (nN2) / (0.92*nN2 + nO2 + 0.56*nO)
 
     # Assigning each ODE to vectorelement
     ne = x[0]
@@ -71,18 +76,33 @@ def odes(qe=1E8, x, t):
     dOplus_dt = qOplus - nOplus * (k1 * nN2 + k2 * nO2 + alphar * ne)
     dO2plus_dt = qO2plus + (k2 * nOplus * nO2 + k6 * nN2plus * nO2 - k3 * nNO * nO2plus - k4 * nN2 * nO2plus)
     dN2plus_dt = qN2plus - nN2plus * (alpha3 * ne + k5 * nO + k6 * nO2)
-    dNO_dt = nO2plus * nN2 * k4 - k3 * nNO * nO2plus
-    dNOplus_dt = k1 * nOplus * nN2 + k3 * nNO * nO2plus + k4 * nO2plus * nN2 + k5 * nN2plus * nO - nNOplus * alpha1 * ne
+    dNO_dt = (nO2plus * nN2 * k4) - (k3 * nNO * nO2plus)
+    dNOplus_dt = (k1 * nOplus * nN2) + (k3 * nNO * nO2plus) + (k4 * nO2plus * nN2) + (k5 * nN2plus * nO) - (nNOplus * alpha1 * ne)
 
-    return np.array([dne_dt, dOplus_dt, dO2plus_dt, dN2plus_dt, dNO_dt, dNOplus_dt])
+    return np.array([dne_dt, dOplus_dt, dO2plus_dt, dN2plus_dt, dNO_dt, dNOplus_dt], dtype=np.float64).squeeze()
 
 # Functions to return initial values
 def initialvalues(index):
     """
     Initial values at a given height
     """
-    return np.array([ne[index], nOplus[index], nO2plus[index], nN2plus[index], nNO[index], nNOplus[index]])
-    
+    return [ne[index], Oplus[index], O2plus[index], N2plus, NO, NOplus[index]]
 
+# Defining time
+t = np.arange(0, 3600, 1)
 
+def solveODEs(h, t, qe):
+    """
+    Solving coupled ODEs using odeint function from scipy
+    Returning solutions in form of an array
+    """
+    solveODE = odeint(odes, initialvalues(h), t, args=(h, qe))
+
+    return solveODE
+
+H110km = solveODEs(10, t, 1E8)
+
+plt.semilogy(t, H110km[:, 0])
+plt.ylim(1E5, 1E13)
+plt.show()
 
